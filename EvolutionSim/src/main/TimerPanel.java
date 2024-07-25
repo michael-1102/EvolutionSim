c33759ca00bb205dbdf0ab50b29661feeda69ff4
michael-1102 <michaelhanley0806@gmail.com>
2024-07-25 12:06:39 -0400

    Added FPS slider

--- EvolutionSim/src/main/TimerPanel.java
+++ EvolutionSim/src/main/TimerPanel.java
@@ -1,6 +1,5 @@
 package main;
 
-import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
@@ -8,10 +7,12 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JPanel;
-
-
+import javax.swing.JSlider;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 import javax.imageio.ImageIO;
 import javax.swing.BorderFactory;
+import javax.swing.BoxLayout;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JLabel;
@@ -25,19 +26,26 @@ public class TimerPanel extends JPanel implements ActionListener {
 	private final static int dayLength = 4320; // amount of time in a day (and amount of time in a night)
 	private final static int fullDayLength = dayLength * 2;
 	
+	private final static int fpsMin = 1;
+	private final static int fpsMax = 200;
+	private final static int fpsInterval = 10;
+	private final static int fpsInitial = fpsMin+fpsInterval*4;
+
+	
 	private final static int dayCutOff = 8; // cutoff of when it is day time
 	
-	Image pauseImg; //pause button image
-	Image pauseHoverImg; //pause button image when button is hovered
+	private Image pauseImg; //pause button image
+	private Image pauseHoverImg; //pause button image when button is hovered
 	
-	Image playImg; //pause button image
-	Image playHoverImg; //pause button image when button is hovered
+	private Image playImg; //pause button image
+	private Image playHoverImg; //pause button image when button is hovered
 	
 	private int time; // time of day
 	private int dayCount; // day count (starts at 1)
 	private JLabel timeLabel;
 	private JLabel dayLabel;
 	private JButton pauseButton;
+	private JSlider fpsSlider;
 	private GlobalData globalData;
 
 	public TimerPanel() {
@@ -47,21 +55,37 @@ public class TimerPanel extends JPanel implements ActionListener {
 		this.setPreferredSize(new Dimension(globalData.screenWidth, globalData.tileSize*2));
 		this.setBackground(Color.blue);
 		this.setBorder(BorderFactory.createLineBorder(Color.black));
-		this.setLayout(new BorderLayout());
+		this.setLayout(new BoxLayout(this, BoxLayout.LINE_AXIS));
 		
 		timeLabel = new JLabel("00:00");
 		timeLabel.setHorizontalAlignment(JLabel.LEFT);
 		timeLabel.setVerticalAlignment(JLabel.CENTER);
 		timeLabel.setFont(new Font(Font.MONOSPACED, Font.BOLD, globalData.tileSize*2));
 		timeLabel.setForeground(Color.yellow);
-		this.add(timeLabel, BorderLayout.WEST);
+		this.add(timeLabel);
 		
 		dayLabel = new JLabel("Day " + dayCount);
 		dayLabel.setHorizontalAlignment(JLabel.CENTER);
 		dayLabel.setVerticalAlignment(JLabel.CENTER);
 		dayLabel.setFont(new Font(Font.MONOSPACED, Font.BOLD, (int)(globalData.tileSize*1.5)));
 		dayLabel.setForeground(Color.yellow);
-		this.add(dayLabel, BorderLayout.CENTER);
+		this.add(dayLabel);
+		
+		fpsSlider = new JSlider(JSlider.HORIZONTAL, 0, fpsMax/fpsInterval, fpsInitial/fpsInterval);
+		fpsSlider.setMinorTickSpacing(1);
+		fpsSlider.setPaintTicks(true);
+
+		fpsSlider.addChangeListener((new ChangeListener() {
+		    @Override
+		    public void stateChanged(ChangeEvent evt) {
+		        if (!fpsSlider.getValueIsAdjusting()) {
+		        	int fps = (int) (fpsInterval * Math.ceil(1.0*(fpsMax - fpsMin) / fpsMax) * fpsSlider.getValue() + 1);
+		            globalData.setUnpausedFPS(fps);
+		        }
+		    }
+		}));
+
+		this.add(fpsSlider);
 		
 		try {
 			pauseImg = ImageIO.read(getClass().getResource("/resources/pause.png"));
@@ -107,7 +131,11 @@ public class TimerPanel extends JPanel implements ActionListener {
 		    	}
 		    }
 		});
-		this.add(pauseButton, BorderLayout.EAST);
+		this.add(pauseButton);
+	}
+	
+	public static int getInitialFPS() {
+		return fpsInitial;
 	}
 	
 	public void actionPerformed(ActionEvent e){  
